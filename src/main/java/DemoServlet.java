a2f55c96f5334b529c45d7744d4dd4c77d0e722d
tbechtoldbcs <tobias.bechtold@smail.inf.h-brs.de>
2019-04-23 18:05:47 +0200

    Added doGet sum

--- src/main/java/DemoServlet.java
+++ src/main/java/DemoServlet.java
@@ -1,4 +1,28 @@
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class DemoServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String[] requestParamString = req.getQueryString().split("=");
+        String command = requestParamString[0];
+        String param = requestParamString[1];
+
+        if ( "fun".equals( command ) && "sum".equals( param ) ){
+            Float sum = getPersistentSum();
+
+            resp.setContentType("text/html");
+            PrintWriter out = resp.getWriter();
+            out.println( sum );
+
+            System.out.println( "sum = " + sum );
+        } else {
+            System.out.println( "Invalid Command: " + req.getQueryString() );
+        }
+    }
 }
